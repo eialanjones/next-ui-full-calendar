9b2c356c718ce33d734b2f49e6ce26973d6ba91b
Alan Jones Rios <alan.jones.rios@gmail.com>
2025-01-17 22:34:50 -0300

    🔧 chore: atualizado configuração do tsup •⁠  ⁠adicionadas opções de entrada e resolução para dts •⁠  ⁠ajustado moduleResolution para 'node'

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,11 +3,19 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
     entry: ['app/index.ts'],
     format: ['esm', 'cjs'],
-    dts: true,
+    dts: {
+        entry: {
+            index: 'app/index.ts'
+        },
+        resolve: true,
+        compilerOptions: {
+            moduleResolution: 'node'
+        }
+    },
     splitting: false,
     sourcemap: false,
     clean: true,
     minify: true,
     treeshake: true,
     target: 'es2020',
-})
+});
